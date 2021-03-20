e5b0564df66a63771785087864646cdbd023a84a
Fredrik <FreddeFc@gmail.com>
2021-03-20 17:55:54 +0100

    Updated Download Linear Weights Download Path

--- get-weights.sh
+++ get-weights.sh
@@ -7,6 +7,6 @@ mkdir -p $OUTPATH
 URLSWECLIP=https://www.dropbox.com/s/s77xw5308jeljlp/Swedish-500k%20Linear%20Weights.pkl
 wget -c "${URLSWECLIP}" -P $OUTPATH
 
-URLMCLIP=https://www.dropbox.com/s/r5y6ra150b8hb1o/M-BERT%20Distil%2040%20Linear%20Weights.pkl
+URLMCLIP=https://www.dropbox.com/s/oihqzctnty5e9kk/M-BERT%20Distil%2040%20Linear%20Weights.pkl
 wget -c "${URLMCLIP}" -P $OUTPATH
 
