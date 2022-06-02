3a9098af9d749747b021f91752b0216a6fd4c387
Romain Beaumont <romain.rom1@gmail.com>
2022-06-02 10:07:46 +0200

    Multilingual clip (#15)
    * Update setup.py
    
    * Update README.md

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     REQUIREMENTS = _read_reqs("requirements.txt")
 
     setup(
-        name="m-clip",
+        name="multilingual-clip",
         packages=find_packages(),
         include_package_data=True,
         version="1.0.1",
