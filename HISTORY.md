38070fa264a16b39d6a1d7aa57fc7482a3efbd99
Romain Beaumont <romain.rom1@gmail.com>
2022-06-02 23:05:51 +0200

    Release 1.0.6

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,7 @@
+## 1.0.6
+
+* small fix
+
 ## 1.0.5
 
 * small fix
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         name="multilingual_clip",
         packages=find_packages(),
         include_package_data=True,
-        version="1.0.5",
+        version="1.0.6",
         license="MIT",
         description="OpenAI CLIP text encoders for multiple languages!",
         long_description=long_description,
